e41ea30143e1441413df593a6245ba42641be5a2
Charles Roper <charles@roper.im>
2020-05-17 19:11:45 +0100

    Day7: more refactorings

--- Day07-CharacterCount/script.js
+++ Day07-CharacterCount/script.js
@@ -1,8 +1,12 @@
-function updateCharacterCount(event) {
-  if (!event.type == 'input' || !event.type === 'DOMContentLoaded') return;
-  var counter = document.querySelector('[data-count-for]');
-  var targetId = counter.getAttribute('data-count-for');
-  var target = document.querySelector(targetId);
+// The id of the textarea to count (the target) is stored as a selector in
+// the data-count-for attribute. Have a look at the HTML.
+var counter = document.querySelector('[data-count-for]');
+var targetId = counter.getAttribute('data-count-for');
+var target = document.querySelector(targetId);
+
+function updateCharacterCount() {
+  if (!target.type === 'textarea') return;
+
   var charCount = target.value.length;
   counter.textContent = charCount;
 }
