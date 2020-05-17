df4770d8e3b65b74256042d1057b24e5b0b1cbf2
Charles Roper <charles@roper.im>
2020-05-17 19:29:36 +0100

    Day7: some comments

--- Day07-CharacterCount/script.js
+++ Day07-CharacterCount/script.js
@@ -1,7 +1,16 @@
 // The id of the textarea to count (the target) is stored as a selector in
-// the data-count-for attribute. Have a look at the HTML.
+// the data-count-for attribute. Have a look at the HTML. This means I don't
+// have to hard-code the #text selector into my JS - I declare it in HTML, which
+// I think is more in keeping with the Web's grain[1].
+
+// This gets the element to store the counter
 var counter = document.querySelector('[data-count-for]');
+
+// The counter element has an attribute that contains a selector for the target
+// textarea
 var targetId = counter.getAttribute('data-count-for');
+
+// This gets the textarea element using the targetId selector
 var target = document.querySelector(targetId);
 
 function updateCharacterCount() {
@@ -13,3 +22,5 @@ function updateCharacterCount() {
 
 window.addEventListener('input', updateCharacterCount, false);
 window.addEventListener('DOMContentLoaded', updateCharacterCount, false);
+
+// [1] The web's grain: https://frankchimero.com/blog/2015/the-webs-grain/
