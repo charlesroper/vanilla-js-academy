1543cdb085f2d51ab6f722fdb8ddf5b69662598e
Charles Roper <charles@roper.im>
2020-05-22 10:22:42 +0100

    Day9: fix regex.
    Compound-words (i.e., words with hypens) counted as two words. Fixed so
    they now count as one.

--- Day09-CharacterAndWordCount/script.js
+++ Day09-CharacterAndWordCount/script.js
@@ -17,7 +17,7 @@ function updateCharacterCount() {
 
 function updateWordCount() {
   if (!wordCountTarget.type === 'textarea') return;
-  var wordsArray = wordCountTarget.value.match(/\w+('\w+)*/g) || [];
+  var wordsArray = wordCountTarget.value.match(/\w+(['-]\w+)*/g) || [];
   console.log(wordsArray);
   wordCounter.textContent = wordsArray.length;
 }
