a364125eef761bf98c91a53ece057e9cc1e6df8d
Charles Roper <charles@roper.im>
2020-06-06 16:05:40 +0100

    Day 23: move data array to end of script

--- Day23-MonsterShuffle/script.js
+++ Day23-MonsterShuffle/script.js
@@ -1,3 +1,55 @@
+var monstersGrid = document.getElementById("monsters");
+var shuffleButton = document.querySelector("button");
+
+shuffleButton.addEventListener("click", shuffleMonsters);
+document.addEventListener("DOMContentLoaded", shuffleMonsters);
+
+// FUNCTIONS ============================================================
+
+function shuffleMonsters() {
+  shuffle(monsters);
+  var monstersGridItems = monsters
+    .map(function(monster) {
+      return (
+        '<div class="grid"><img alt="' +
+        monster.alt +
+        '" src="images/' +
+        monster.name +
+        '.svg"></div>'
+      );
+    })
+    .join("");
+
+  monstersGrid.innerHTML = monstersGridItems;
+}
+
+/**
+ * Randomly shuffle an array
+ * https://stackoverflow.com/a/2450976/1293256
+ * @param  {Array} array The array to shuffle
+ * @return {String}      The first item in the shuffled array
+ */
+function shuffle(array) {
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// DATA ========================================================================
+
 // The monsters and socks
 var monsters = [
   {
@@ -50,52 +102,3 @@ var monsters = [
   },
   { name: "sock", alt: "A pair of smell old socks" }
 ];
-
-var monstersGrid = document.getElementById("monsters");
-var shuffleButton = document.querySelector("button");
-shuffleButton.addEventListener("click", shuffleMonsters);
-document.addEventListener("DOMContentLoaded", shuffleMonsters);
-
-// FUNCTIONS ============================================================
-
-function shuffleMonsters() {
-  shuffle(monsters);
-  var monstersGridItems = monsters
-    .map(function(monster) {
-      return (
-        '<div class="grid"><img alt="' +
-        monster.alt +
-        '" src="images/' +
-        monster.name +
-        '.svg"></div>'
-      );
-    })
-    .join("");
-
-  monstersGrid.innerHTML = monstersGridItems;
-}
-
-/**
- * Randomly shuffle an array
- * https://stackoverflow.com/a/2450976/1293256
- * @param  {Array} array The array to shuffle
- * @return {String}      The first item in the shuffled array
- */
-function shuffle(array) {
-  var currentIndex = array.length;
-  var temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
