d955d3cb7f983d054ab4bd1b358f5aea2c4765fd
Charles Roper <charles@roper.im>
2020-06-27 15:14:58 +0100

    Day 31: rename variables to be easier to grok

--- Day31-TableOfContentsMissingIDs/script.js
+++ Day31-TableOfContentsMissingIDs/script.js
@@ -2,20 +2,20 @@ var h2s = Array.from(document.querySelectorAll("h2"));
 var fragment = new DocumentFragment();
 
 h2s.forEach(function(h2) {
-  var link = document.createElement("a");
+  var a = document.createElement("a");
   var regex = /[\s'"]+/gm;
 
   if (!h2.id) h2.id = h2.innerText.replace(regex, "-");
 
-  link.href = "#" + h2.id;
-  link.innerText = h2.innerText;
+  a.href = "#" + h2.id;
+  a.innerText = h2.innerText;
 
-  var listItem = document.createElement("li");
-  listItem.appendChild(link);
+  var li = document.createElement("li");
+  li.appendChild(a);
 
-  fragment.appendChild(listItem);
+  fragment.appendChild(li);
 });
 
-var list = document.createElement("ul");
-list.appendChild(fragment);
-document.getElementById("table-of-contents").appendChild(list);
+var ul = document.createElement("ul");
+ul.appendChild(fragment);
+document.getElementById("table-of-contents").appendChild(ul);
