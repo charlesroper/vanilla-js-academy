650cef9f476794efc9c042251d02d98c294c4290
Charles Roper <charles@roper.im>
2020-06-27 15:59:40 +0100

    Use prettier single quote

--- Day31-TableOfContentsMissingIDs/script.js
+++ Day31-TableOfContentsMissingIDs/script.js
@@ -1,21 +1,21 @@
-var h2s = Array.from(document.querySelectorAll("h2"));
+var h2s = Array.from(document.querySelectorAll('h2'));
 var fragment = new DocumentFragment();
 
 h2s.forEach(function(h2) {
   var regex = /[\s'"]+/gm;
 
-  if (!h2.id) h2.id = h2.innerText.replace(regex, "-");
+  if (!h2.id) h2.id = h2.innerText.replace(regex, '-');
 
-  var a = document.createElement("a");
-  a.href = "#" + h2.id;
+  var a = document.createElement('a');
+  a.href = '#' + h2.id;
   a.innerText = h2.innerText;
 
-  var li = document.createElement("li");
+  var li = document.createElement('li');
   li.appendChild(a);
 
   fragment.appendChild(li);
 });
 
-var ul = document.createElement("ul");
+var ul = document.createElement('ul');
 ul.appendChild(fragment);
-document.getElementById("table-of-contents").appendChild(ul);
+document.getElementById('table-of-contents').appendChild(ul);
