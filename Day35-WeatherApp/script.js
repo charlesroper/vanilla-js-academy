b71ca256ca683c7149bb5c4dafea6e4ffb740291
Charles Roper <charles@roper.im>
2020-06-28 23:04:16 +0100

    Day 35: tidy up the code

--- Day35-WeatherApp/script.js
+++ Day35-WeatherApp/script.js
@@ -1,42 +1,10 @@
 (function() {
-  const latlong = getLatLong();
-  const weatherData = getWeatherData(latlong);
-  renderWeather(weatherData).catch(handleError);
-
-  async function getLatLong() {
-    const response = await fetch('https://ipapi.co/json/');
-    if (response.ok) {
-      const json = await response.json();
-      return [json.latitude, json.longitude];
-    } else {
-      return Promise.reject(response);
-    }
-  }
-
-  async function getWeatherData(latlong) {
-    const [lat, lon] = await latlong;
-    const API = {
-      endpoint: 'https://api.weatherbit.io/v2.0/current',
-      key: 'cba47487888d4b808b4e0d062f0705f7'
-    };
-    const response = await fetch(
-      `${API.endpoint}?lat=${lat}&lon=${lon}&key=${API.key}`
-    );
-
-    if (response.ok) {
-      const json = await response.json();
-      return json.data[0];
-    } else {
-      return Promise.reject(response);
-    }
-  }
-
-  async function renderWeather(weatherData) {
-    const data = await weatherData;
-    const app = document.getElementById('app');
-    console.log(data);
-
-    const dateTimeOptions = {
+  const CONFIG = {
+    ipapi: 'https://ipapi.co/json/',
+    weatherbit: 'https://api.weatherbit.io/v2.0/current',
+    weatherbitIcons: 'https://www.weatherbit.io/static/img/icons/',
+    key: 'cba47487888d4b808b4e0d062f0705f7',
+    dateTimeOptions: {
       weekday: 'long',
       year: 'numeric',
       month: 'long',
@@ -44,21 +12,47 @@
       hour: 'numeric',
       minute: 'numeric',
       hour12: true
-    };
+    }
+  };
 
+  const latlong = getLatLong();
+  const weatherData = getWeatherData(latlong);
+  renderWeather(weatherData).catch(handleError);
+
+  async function getLatLong() {
+    const response = await fetch(CONFIG.ipapi);
+    if (!response.ok) return Promise.reject(response);
+    const json = await response.json();
+    return [json.latitude, json.longitude];
+  }
+
+  async function getWeatherData(latlong) {
+    const [lat, lon] = await latlong;
+    const response = await fetch(
+      `${CONFIG.weatherbit}?lat=${lat}&lon=${lon}&key=${CONFIG.key}`
+    );
+    if (!response.ok) return Promise.reject(response);
+    const json = await response.json();
+    return json.data[0];
+  }
+
+  async function renderWeather(weatherData) {
+    const data = await weatherData;
+    const app = document.getElementById('app');
     app.innerHTML = /*html*/ `
       <h2 class="weatherTitle">
         Weather for ${s(data.city_name)}, ${s(data.country_code)}
       </h2>
       <time>
-        ${new Intl.DateTimeFormat('default', dateTimeOptions).format(
+        ${new Intl.DateTimeFormat('default', CONFIG.dateTimeOptions).format(
           Date.parse(data.ob_time)
         )}
       </time>
       <div class="weatherCard">
-        <img src="https://www.weatherbit.io/static/img/icons/${s(
-          data.weather.icon
-        )}.png" width="50" height="50" />
+        <img 
+          src="${CONFIG.weatherbitIcons}${s(data.weather.icon)}.png"
+          width="50" height="50"
+        />
         <p class="weatherCard temp">${s(data.temp)} <sup>&deg;C</sup></p>
       </div>
       <p>${s(data.weather.description)}</p>
